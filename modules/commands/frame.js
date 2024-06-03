d1ec0721bc45f97a4d14b9f2728e5af990ee14dc
SHAWON SHEIKH <nobitarobot10@gmail.com>
2024-06-03 10:17:05 +0600

    Update frame.js

--- modules/commands/frame.js
+++ modules/commands/frame.js
@@ -69,6 +69,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "`💜😇😇😇", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "`ɴᴏʙɪᴛᴀ-ᴄʜᴀᴛ ʙᴏᴛ ᴀᴘɪ...✨💖", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
                                       }
