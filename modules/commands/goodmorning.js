3b73e405e1bcf2b5387e2349d8b864792ed922db
SHAWON SHEIKH <nobitarobot10@gmail.com>
2024-06-03 10:56:02 +0600

    Update goodmorning.js

--- modules/commands/goodmorning.js
+++ modules/commands/goodmorning.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "good morning",
   version: "10.0.2",
   hasPermssion: 0,
-  credits: "RAHAT", 
+  credits: "NOBITa", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("Gm")==0 || event.body.indexOf("Mrng")==0 || event.body.indexOf("GM")==0 || event.body.indexOf("gm")==0 || event.body.indexOf("morning")==0 || event.body.indexOf("Morning")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("Good Morning")==0 || event.body.indexOf("good morning")==0 || event.body.indexOf("GOOD MORNING")==0 ) { 
     var msg = {
-        body: `<3 Morning too Darling ${name} ❤️`
+        body: `<3 ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ᴛᴏᴏ ᴅᴀʀʟɪɴɢ ${name} ❤️`
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
