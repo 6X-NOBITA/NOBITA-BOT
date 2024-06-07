c52b31c1d79ec8cb89b52a1b719428b9bf470b63
SHAWON SHEIKH <nobitarobot10@gmail.com>
2024-06-07 20:18:03 +0600

    Update pair3.js

--- modules/commands/pair3.js
+++ modules/commands/pair3.js
@@ -66,9 +66,9 @@ module.exports.run = async function ({ event, api, args }) {
   const fs = global.nodemodule["fs-extra"];
   const { threadID, messageID, senderID } = event;
   const mention = Object.keys(event.mentions);
-  if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
+  if (!mention[0]) return api.sendMessage("ᴘʟᴇᴀꜱᴇ ᴍᴇɴᴛɪᴏɴ 1 ᴘᴇʀꜱᴏɴ.", threadID, messageID);
   else {
       const one = senderID, two = mention[0];
-      return makeImage({ one, two }).then(path => api.sendMessage({ body: "𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐩𝐚𝐢𝐫𝐢𝐧𝐠\n  ༺𝙒𝙞𝙨𝙝 𝙮𝙤𝙪 𝙩𝙬𝙤 𝙝𝙪𝙣𝙙𝙧𝙚𝙙 𝙮𝙚𝙖𝙧𝙨 𝙤𝙛 𝙝𝙖𝙥𝙥𝙞𝙣𝙚𝙨𝙨༻\n━━━━━━━━━━━━━━━━\n*★᭄𝗖𝗿𝗲𝗱𝗶𝘁𝘀  ཫ༄𒁍≛⃝Khan Rahul RK", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+      return makeImage({ one, two }).then(path => api.sendMessage({ body: "ᴘᴀɪʀɪɴɢ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ🦋❤️\n  ༺ᴠᴀʟᴏʙᴀꜱʜᴀ ꜱᴜɴᴅᴏʀ◉‿◉ 😘༻\n━━━━━━━━━━━━━━━━\n*★᭄𝗖𝗿𝗲𝗱𝗶𝘁𝘀  ཫ༄𒁍≛⃝ꜱʜᴀᴡᴏɴ ꜱʜᴇɪᴋʜ", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
   }
     }
